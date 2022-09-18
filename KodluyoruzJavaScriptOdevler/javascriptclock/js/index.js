75afe234c2cc3afd91f5b3eeb1d99b3b906e1d9b
MuratCihan <112008349+MuratCihan@users.noreply.github.com>
2022-09-18 16:28:20 +0300

    Güncelleme

---
+++ KodluyoruzJavaScriptOdevler/javascriptclock/js/index.js
@@ -0,0 +1,12 @@
+let Name = prompt("Adınızı Giriniz ")
+let nameText = Name
+let info = document.querySelector("#myName")
+info.innerHTML = `${nameText}`
+
+
+function tarihSaat() {
+    let date = `${new Date().getHours()}:${new Date().getMinutes()}:${new Date().getSeconds()} ${new Date().toLocaleDateString('tr-TR', {weekday: 'long'})}`//new Date kullanılarak saat dk ve saniye alınmıştır sonrasında tolocaledtastring ile gün alınmıştır
+    document.getElementById("myClock").innerHTML = `${date}` // date myClock id sine bağlanmıştır
+}
+setInterval(tarihSaat, 1000); // her 1 saniyede tarihSaat fonksiyonunu yeniden çalıştır
+
